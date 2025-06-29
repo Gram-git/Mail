bd075869a5077f4196c1f9ba8661bce62035cae6
Ram <ral789316@gmail.com>
2025-06-29 17:55:24 +0500

    add RecycleView in MainActivity

---
+++ app/src/main/java/com/example/mail/MailAdapter.kt
@@ -0,0 +1,12 @@
+package com.example.mail
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mail.controller.MailController
+
+class MailAdapter(controller????????) : RecyclerView.Adapter<MailAdapter.ViewHolder>() {
+    class ViewHolder {
+
+    }
+
+}
--- app/src/main/java/com/example/mail/MainActivity.kt
+++ app/src/main/java/com/example/mail/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.mail.controller.MailController
 
 class MainActivity : AppCompatActivity() {
@@ -16,5 +18,10 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        val mailAdapter = MailAdapter(controller)
+
+        val recyclerView: RecyclerView = findViewById(R.id.rcView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = MailAdapter
     }
 }
