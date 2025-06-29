41b8ab879982066de3d67beb5c8d2c452303cea6
Ram <ral789316@gmail.com>
2025-06-29 23:30:07 +0500

    fix beta

--- app/src/main/java/com/example/mail/MailAdapter.kt
+++ app/src/main/java/com/example/mail/MailAdapter.kt
@@ -1,19 +1,23 @@
 package com.example.mail
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.graphics.drawable.toDrawable
 import androidx.recyclerview.widget.RecyclerView
-import com.example.mail.controller.MailController
+import com.example.mail.presentation.model.MailHolderUiModel
 
-class MailAdapter(private val dataset:) : RecyclerView.Adapter<MailAdapter.ViewHolder>() {
+class MailAdapter(private val dataSet: List<MailHolderUiModel>) :
+    RecyclerView.Adapter<MailAdapter.ViewHolder>() {
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val senderName: TextView
         val messageTitle: TextView
         val message: TextView
         val date: TextView
         val avatarIcon: ImageView
-        val iconBookmarked: ImageView
+//        val iconBookmarked: ImageView
 
         init {
             senderName = view.findViewById(R.id.senderName)
@@ -21,14 +25,26 @@ class MailAdapter(private val dataset:) : RecyclerView.Adapter<MailAdapter.ViewH
             message = view.findViewById(R.id.message)
             date = view.findViewById(R.id.date)
             avatarIcon = view.findViewById(R.id.avatarIconRes)
-            iconBookmarked = view.findViewById(R.id.iconBookmarked)
+//            iconBookmarked = view.findViewById(R.id.iconBookmarked)
         }
     }
+
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(viewGroup.context)
-            .inflate(R.layout.text_row_item, viewGroup, false)
+            .inflate(R.layout.item_list_mail, viewGroup, false)
 
         return ViewHolder(view)
     }
+
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+        viewHolder.senderName.text = dataSet[position].sender.name
+        viewHolder.messageTitle.text = dataSet[position].messageTitle
+        viewHolder.message.text = dataSet[position].message
+        viewHolder.date.text = dataSet[position].date
+        viewHolder.avatarIcon.setImageResource(dataSet[position].sender.fallbackAvatarRes)
+//        viewHolder.iconBookmarked.drawable = dataSet[position].isBookmarked
+    }
+
+    override fun getItemCount() = dataSet.size
 
 }
--- app/src/main/java/com/example/mail/MainActivity.kt
+++ app/src/main/java/com/example/mail/MainActivity.kt
@@ -3,8 +3,6 @@ package com.example.mail
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mail.controller.MailController
