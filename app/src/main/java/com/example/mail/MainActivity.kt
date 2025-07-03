1c9b27b6851f284d8123a0a561fc85f39a057dc5
Ram <ral789316@gmail.com>
2025-07-04 00:44:28 +0500

    fix

--- app/src/main/java/com/example/mail/MainActivity.kt
+++ app/src/main/java/com/example/mail/MainActivity.kt
@@ -6,22 +6,19 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.mail.controller.MailController
+import com.example.mail.presentation.model.RecycleFragment
 
 class MainActivity : AppCompatActivity() {
 
-    private val controller = MailController()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-        val dataSet = controller.loadMailsList()
-        val mailAdapter = MailAdapter(dataSet)
-
-        val recyclerView: RecyclerView = findViewById(R.id.rcView)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = mailAdapter
-
+        supportFragmentManager
+            .beginTransaction()
+            .replace(R.id.place_holder, RecycleFragment.newInstance())
+            .commit()
     }
 }
---
+++ app/src/main/java/com/example/mail/presentation/model/RecycleFragment.kt
@@ -0,0 +1,39 @@
+package com.example.mail.presentation.model
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.mail.MailAdapter
+import com.example.mail.R
+import com.example.mail.controller.MailController
+
+
+class RecycleFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_recycle, container, false)
+    }
+
+    private val controller = MailController()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val dataSet = controller.loadMailsList()
+        val mailAdapter = MailAdapter(dataSet)
+
+        val recyclerView: RecyclerView = view.findViewById(R.id.rcView)
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+        recyclerView.adapter = mailAdapter
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = RecycleFragment()
+    }
+}
