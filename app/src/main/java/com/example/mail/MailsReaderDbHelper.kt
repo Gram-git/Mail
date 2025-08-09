bdb035e61243e8737db24d6443d07d2a039b2852
Ram <ral789316@gmail.com>
2025-08-09 21:00:46 +0500

    intermediate stage 1.2.0

--- app/src/main/java/com/example/mail/MailsReaderDbHelper.kt
+++ app/src/main/java/com/example/mail/MailsReaderDbHelper.kt
@@ -13,9 +13,47 @@ class MailsReaderDbHelper(context: Context) :
     SQLiteOpenHelper(context, "Mail.db", null, 1) {
 
     private val coroutineScope = CoroutineScope(Dispatchers.IO)
-    private val SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS table_name"
+    private val SQL_DELETE_ENTRIES_MAILS = "DROP TABLE IF EXISTS mails"
+    private val SQL_DELETE_ENTRIES_USERS = "DROP TABLE IF EXISTS users"
 
     override fun onCreate(db: SQLiteDatabase) {
+        val table_name = "mails"
+        val table_name2 = "users"
+
+        val CREATE_QUERY = "CREATE TABLE $table_name (" +
+                "id INTEGER UNIQUE PRIMARY KEY NOT NULL," +
+                "senderId INTEGER," +
+                "messageTitle TEXT," +
+                "message TEXT," +
+                "isBookmarked NUMERIC," +
+                "date INTEGER," +
+                "isRead NUMERIC," +
+                ")"
+
+        val CREATE_QUERY2 = "CREATE TABLE $table_name2 (" +
+                "id INTEGER UNIQUE PRIMARY KEY NOT NULL," +
+                "name TEXT," +
+                "avatarUrl TEXT," +
+                ")"
+
+
+        val values = ContentValues().apply {
+            put("id", 1001)
+            put("senderId", 2)
+            put("messageTitle", "График собраний")
+            put("message", "Проверь, пожалуйста, новую версию документа.")
+            put("isBookmarked", 1)
+            put("date", 1704208722416)
+            put("isRead", 0)
+        }
+
+        val values2 = ContentValues().apply {
+            put("id", 2)
+            put("name", "Борис Петров")
+            putNull("avatarUrl")
+        }
+        db.execSQL(CREATE_QUERY)
+        db.execSQL(CREATE_QUERY2)
         db.insert(table_name, null, values)
         db.insert(table_name2, null, values2)
     }
@@ -23,7 +61,8 @@ class MailsReaderDbHelper(context: Context) :
     override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
         // This database is only a cache for online data, so its upgrade policy is
         // to simply to discard the data and start over
-        db.execSQL(SQL_DELETE_ENTRIES)
+        db.execSQL(SQL_DELETE_ENTRIES_MAILS)
+        db.execSQL(SQL_DELETE_ENTRIES_USERS)
         onCreate(db)
     }
 
@@ -36,38 +75,3 @@ class MailsReaderDbHelper(context: Context) :
     }
 }
 
-val table_name = "mails"
-val table_name2 = "users"
-
-val CREATE_QUERY = "CREATE TABLE $table_name (" +
-        "id INTEGER UNIQUE PRIMARY KEY NOT NULL," +
-        "senderId INTEGER," +
-        "messageTitle TEXT," +
-        "message TEXT," +
-        "isBookmarked NUMERIC," +
-        "date INTEGER," +
-        "isRead NUMERIC," +
-        ""
-
-val CREATE_QUERY2 = "CREATE TABLE $table_name2 (" +
-        "id INTEGER UNIQUE PRIMARY KEY NOT NULL," +
-        "name TEXT," +
-        "avatarUrl TEXT," +
-        ""
-
-
-val values = ContentValues().apply {
-    put("id", "1001")
-    put("senderId", "2")
-    put("messageTitle", "График собраний")
-    put("message", "Проверь, пожалуйста, новую версию документа.")
-    put("isBookmarked", "true")
-    put("date", "1704208722416")
-    put("isRead", "false")
-}
-
-val values2 = ContentValues().apply {
-    put("id", "2")
-    put("name", "Борис Петров")
-    put("avatarUrl", "null")
-}
